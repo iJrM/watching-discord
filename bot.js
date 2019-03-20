258b72ec137c02190f75a7027ecfd987a47aeb11
iJrM <44652718+iJrM@users.noreply.github.com>
2019-03-20 23:25:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,30 +7,30 @@ client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
      
-  if (message.content.startsWith(adminprefix + 'p')) {
+  if (message.content.startsWith(prefix + 'p')) {
     client.user.setGame(argresult);
       message.channel.send(`**${argresult}**`)
   } else
-     if (message.content === (adminprefix + "leave")) {
+     if (message.content === (prefix + "leave")) {
     message.guild.leave();        
   } else  
-  if (message.content.startsWith(adminprefix + 'w')) {
+  if (message.content.startsWith(prefix + 'w')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`{argresult}**`)
   } else
-  if (message.content.startsWith(adminprefix + 'l')) {
+  if (message.content.startsWith(prefix + 'l')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**${argresult}**`)
   } else
-  if (message.content.startsWith(adminprefix + 'o')) {
+  if (message.content.startsWith(prefix + 'o')) {
     client.user.setGame(argresult, "https://www.twitch.tv/One");
       message.channel.send(`**${argresult}`)
   }
-  if (message.content.startsWith(adminprefix + 'setname')) {
+  if (message.content.startsWith(prefix + 'setname')) {
   client.user.setUsername(argresult).then
       message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
+if (message.content.startsWith(prefix + 'setavatar')) {
   client.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
